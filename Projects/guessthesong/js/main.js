a5c1d0e8648e58c15a5a2e053e330dd7569bc8b1
jmilkman <jordanmielke2@hotmail.com>
2024-05-11 21:54:09 -0500

    Adjustment to location of play button.
    Moved play button, and fixed some overshadowing issues so that the play button is fully clickable.

--- Projects/guessthesong/js/main.js
+++ Projects/guessthesong/js/main.js
@@ -4,55 +4,43 @@ const submitButton = document.getElementById('submitButton');
 
 // Play button
 playButton.addEventListener('click', function() {
-    // Add the 'pulse-animation' class
     this.classList.add('pulse-animation');
 
-    // Get the audio element
     const audioPlayer = document.querySelector('audio');
 
-    // Play the audio
     if (audioPlayer) {
         audioPlayer.play();
     }
 
-    // Remove the 'pulse-animation' class after the animation completes
     setTimeout(() => {
         this.classList.remove('pulse-animation');
-    }, 850); // Adjust this time to match the duration of your animation (1.3s = 1300ms)
+    }, 850);
 });
 
 // Skip button
 skipButton.addEventListener('click', function() {
-    // Add the 'pulse-animation' class
     this.classList.add('pulse-animation_skip');
     
-    // Remove the 'pulse-animation' class after the animation completes
     setTimeout(() => {
         this.classList.remove('pulse-animation_skip');
-    }, 850); // Adjust this time to match the duration of your animation (1.3s = 1300ms)
+    }, 850);
 });
 
 // Submit button
 submitButton.addEventListener('click', function() {
-    // Add the 'pulse-animation' class
     this.classList.add('pulse-animation_submit');
     
-    // Remove the 'pulse-animation' class after the animation completes
     setTimeout(() => {
         this.classList.remove('pulse-animation_submit');
-    }, 850); // Adjust this time to match the duration of your animation (1.3s = 1300ms)
+    }, 850);
 });
-
-
 
         async function getAccessToken() {
             const clientId = 'e53d2a0a504a4488800a5908036104b9';
             const clientSecret = '1ed609c44ec14bebbe8fcd6e3c26e897';
             
-            // Base64 encode the client ID and client secret
             const credentials = btoa(`${clientId}:${clientSecret}`);
             
-            // Make a POST request to the Spotify token endpoint
             const response = await fetch('https://accounts.spotify.com/api/token', {
                 method: 'POST',
                 headers: {
@@ -69,13 +57,11 @@ submitButton.addEventListener('click', function() {
         }
 
         async function playRandomSongFromPlaylist() {
-            const playlistURI = '6hO1mHhLjoI3ukhNB5bOMD'; // Replace with your playlist URI
+            const playlistURI = '6hO1mHhLjoI3ukhNB5bOMD';
         
             try {
-                // Get the access token
                 const accessToken = await getAccessToken();
         
-                // Make a request to Spotify API to get the playlist details
                 const response = await fetch(`https://api.spotify.com/v1/playlists/${playlistURI}/tracks?limit=50`, {
                     headers: {
                         'Authorization': `Bearer ${accessToken}`
@@ -88,20 +74,16 @@ submitButton.addEventListener('click', function() {
                 // Filter out tracks with null preview_url
                 const tracksWithPreview = data.items.filter(item => item.track.preview_url !== null);
         
-                // Check if there are any tracks with preview_url in the response
                 if (tracksWithPreview.length > 0) {
-                    // Randomly select a track from the filtered playlist
                     const randomIndex = Math.floor(Math.random() * tracksWithPreview.length);
                     const track = tracksWithPreview[randomIndex].track;
                     const previewUrl = track.preview_url;
         
-                    // Display the song preview
                     const audioPlayer = document.createElement('audio');
                     //audioPlayer.controls = true;
                     audioPlayer.src = previewUrl;
                     console.log(previewUrl);
         
-                    // Add play/pause event listener
                     addPlayPauseEventListener(audioPlayer);
         
                     document.getElementById('song-preview').innerHTML = '';
@@ -111,17 +93,14 @@ submitButton.addEventListener('click', function() {
                 }
             } catch (error) {
                 console.error('Error:', error);
-                document.getElementById('song-preview').innerHTML = 'Error fetching the playlist.';
             }
         }
-        
       
       function addPlayPauseEventListener(audioPlayer) {
           let isPlaying = false;
           let timeinbetween = 1000;
           audioPlayer.addEventListener('play', function() {
               if (!isPlaying) {
-                  // Start playing the audio
                   setTimeout(function() {
                       audioPlayer.pause();
                       isPlaying = false;
@@ -132,6 +111,4 @@ submitButton.addEventListener('click', function() {
           });
       }
 
-
-        // Call the function when the page loads
-        window.onload = playRandomSongFromPlaylist;
+    window.onload = playRandomSongFromPlaylist;
